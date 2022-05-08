b8d24f3ca101a4841ebf6cf1d86e85e8f6b0f3c8
Emma Gale <emmajgale@hotmail.com>
2022-05-08 13:59:56 +0100

    refactor hamburger menu functionality

--- scripts/nav.js
+++ scripts/nav.js
@@ -3,24 +3,29 @@ const menu = document.querySelector(".menu");
 const hamburger = document.querySelector(".hamburger");
 const xIcon = document.querySelector(".xIcon");
 const menuIcon = document.querySelector(".menuIcon");
-const menuLinks = document.querySelectorAll(".menuLink");
 
 // define the toggleMenu function
 function toggleMenu() {
-	if (menu.classList.contains("showMenu")) {
-		menu.classList.remove("showMenu");
+	if (menu.classList.contains("show")) {
+		menu.classList.remove("show");
 		xIcon.style.display = "none";
 		menuIcon.style.display = "block";
 	} else {
-		menu.classList.add("showMenu");
+		menu.classList.add("show");
 		xIcon.style.display = "block";
 		menuIcon.style.display = "none";
 	}
 }
+
+// set aria-expanded true or false on hamburger button
+function setAriaExpanded() {
+	let expanded = hamburger.getAttribute("aria-expanded");
+	console.log();
+	if (expanded == "false") {
+		expanded = "true";
+	} else {
+		expanded = "false";
+	}
+}
 // add a click event listener to the hamburger button
 hamburger.addEventListener("click", toggleMenu);
-
-// hide the menu when a link is clicked on
-menuLinks.forEach(function (menulink) {
-	menulink.addEventListener("click", toggleMenu);
-});
